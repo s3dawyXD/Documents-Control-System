d02a9f5706a70c890f28275acd96373e8568c8cc
Mohamed Saad <m1hamed98@gmail.com>
2022-01-06 18:24:46 +0200

    add second enpoint to get all documents

--- app.py
+++ app.py
@@ -9,8 +9,25 @@ CORS(app)
 mydb_connector = mysql.connector
 
 
+def json_data(cursor):
+    result_list = cursor.fetchall()
+    fields_list = cursor.description
+    if len(result_list) > 0:
+        column_list = []
+        for i in fields_list:
+            column_list.append(i[0])
+        jsonData_list = []
+        for row in result_list:
+            data_dict = {}
+            for i in range(len(column_list)):
+                data_dict[column_list[i]] = row[i]
+            jsonData_list.append(data_dict)
+        return jsonData_list
+    else:
+        return []
 
-@app.route('/add-document', methods=['POST'])
+
+@app.route('/document', methods=['POST'])
 def add_document():
     try:
         # get request data
@@ -21,41 +38,96 @@ def add_document():
         status = body.get('status')
         user = body.get('user')
         content = body.get('content')
-        
-        #create db connection
+
+        # create db connection
         mydb = mysql.connector.connect(host="127.0.0.1", user="root",
-                                password="123", port=3306, database="documents_control_system")
+                                       password="123", port=3306, database="documents_control_system")
         cur = mydb.cursor()
 
-        #insert into document table
+        # insert into document table
         document_insert_sql = f"insert into document (document_type,reciept,reciept_date,status) \
         values (%s,%s,%s,%s);"
-        document_insert_val=(document_type,reciept,reciept_date,status)
-        cur.execute(document_insert_sql,document_insert_val)
+        document_insert_val = (document_type, reciept, reciept_date, status)
+        cur.execute(document_insert_sql, document_insert_val)
         mydb.commit()
-        # output = cur.fetchall()
         document_id = cur.lastrowid
 
-        #insert into draft table
+        # insert into draft table
         draft_insert_sql = f"insert into draft (content,user,document_id) \
         values (%s,%s,%s);"
-        draft_insert_val=(content,user,document_id)
-        cur.execute(draft_insert_sql,draft_insert_val)
+        draft_insert_val = (content, user, document_id)
+        cur.execute(draft_insert_sql, draft_insert_val)
         mydb.commit()
         draft_id = cur.lastrowid
 
-
         cur.close()
-        # print(sql)
         return jsonify({
             'success': True,
             'document_id': document_id,
-            'draft_id':draft_id
+            'draft_id': draft_id
 
         }), 200
     except:
         abort(400)
+    finally:
+        mydb.close()
 
 
+@app.route('/document', methods=['GET'])
+def get_document():
+    try:
+        mydb = mysql.connector.connect(host="127.0.0.1", user="root",
+                                       password="123", port=3306, database="documents_control_system")
+        cur = mydb.cursor()
+        document_get_sql = "select document.*,draft.draft_id,draft.content,draft.user from document \
+        left join draft on document.document_id = draft.document_id;"
+        cur.execute(document_get_sql)
+
+        output = json_data(cur)
+
+        cur.close()
+        return jsonify({
+            'success': True,
+            'data': output
+        }), 200
+    except:
+        abort(400)
+    finally:
+        mydb.close()
+
+
+@app.route('/circulate', methods=['POST'])
+def send_document():
+    try:
+        body = request.get_json()
+        draft_id = body.get("draft_id")
+        sender = body.get("from")
+        recever = body.get("to")
+        if draft_id == None or sender == None or recever == None:
+            abort(400)
+        
+
+        mydb = mysql.connector.connect(host="127.0.0.1", user="root",
+                                       password="123", port=3306, database="documents_control_system")
+        cur = mydb.cursor()
+
+        copy_insert_sql = "insert into copies(from,to,draft_id) \
+            values(%s,%s,%s);"
+        copy_insert_val = (sender,recever,draft_id)
+        cur.execute(copy_insert_sql, copy_insert_val)
+        mydb.commit()
+        copy_id = cur.lastrowid
+        cur.close()
+        return jsonify({
+            'success': True,
+            'copy_id': copy_id
+        }), 200
+    except:
+        abort(400)
+    finally:
+        mydb.close()
+
+
+        
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
